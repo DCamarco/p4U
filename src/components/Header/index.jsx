650f59d5d475bd0900ed3bac009ee0f021881b12
Cypher <doccamarco@gmail.com>
2024-04-13 17:58:45 -0400

    Initial commit with Vite

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+	return (
+		<div className='container header'>
+			<img
+				className='cat'
+				src='https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg'
+				alt='our cat for creativeness'
+			/>
+			<div className='header'>Here is meowr header! Meowhahah</div>
+		</div>
+	);
+};
+
+export default Header;
