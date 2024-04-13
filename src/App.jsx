a3868a9aaee3a9ab52eb933c9302ac27c268476b
Cypher <doccamarco@gmail.com>
2024-04-13 18:17:47 -0400

    Initial commit with Vite

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Example from './components/Example';
 import States from './components/States';
 
 function App() {
-  // const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
   return (
     <>
