2a5b902bf435d6d1408563ed37f092032480be82
Cypher <doccamarco@gmail.com>
2024-04-15 16:27:59 -0400

    Implemented problem 1 solution

--- modelData/example.js
+++ modelData/example.js
@@ -14,6 +14,7 @@ if (models === undefined) {
 
 models.exampleModel = function () {
   return {
-    name: "Unknown Name",
+    name: "My First Name",
+    motto: "...Super Awesome Motto..."
   };
 };
